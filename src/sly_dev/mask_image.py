05959ef2cb3e16d40d91ecd398901b87f2bfbbbf
cxnt <pavelbartsits@gmail.com>
2021-10-06 13:28:49 +0300

    WIP add bbox crop

--- src/sly_dev/mask_image.py
+++ src/sly_dev/mask_image.py
@@ -7,7 +7,7 @@ def get_mask_from_clicks(image_np, clicks_list):
     for click in clicks_list:
         g.controller.add_click(click.coords[1], click.coords[0], click.is_positive)
     res_mask = g.controller.result_mask
-    is_empty = np.all((res_mask == 0))
-    if is_empty:
-        Exception("Empty BITMAP")
+    #is_empty = np.all((res_mask == 0))
+    #if is_empty:
+    #    Exception("Empty BITMAP")
     return res_mask
